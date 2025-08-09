85b85a8d9e2574fefdbeceaa2043ee52445c1343
Bakare Samuel Oluwadamilola <49197462+itsxdamdam@users.noreply.github.com>
2025-08-09 16:34:03 +0100

    not-found page

---
+++ src/app/(root)/not-found/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="main-container text-center py-10 font-extrabold text-3xl">
+      As how naaa ):
+    </div>
+  );
+};
+
+export default NotFound;
